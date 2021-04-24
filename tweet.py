89235a685ab650f288b7a2a5095b47acaecfd65b
yoohannes <yohannesgidey420@gmail.com>
2021-04-24 18:13:34 +0200

    added runtime.txt

--- tweet.py
+++ tweet.py
@@ -1,5 +1,6 @@
 import tweepy
 import time
+
 api_key="Z8F2Jo1lcjqNlAw2Lpj9NXRFo"
 api_secret_key="cSnXgx9S9y5NJiIDV4pia9Wm0XRg4HT7E2WUlabzJShys3AizR"
 acess_token="1251029127366430722-lIeDaB08z2BGlFsTjWXQzf7Wa0nX1C"
@@ -19,7 +20,7 @@ def qouteHASH(hastag,items):
             tweet.retweet()
             likecount+=1
             print(str(likecount)+"tweets tweeted")
-            time.sleep(10)
+            time.sleep(20)
         except tweepy.TweepError as e:
             print(e.reason)
         except StopIteration:
@@ -31,8 +32,8 @@ for users in tweepy.Cursor(api.friends).items():
         #print(users.name)
         userid.append(users.id)
 def likeNretweet(userid):
-    for user in userid:
-        for tweet in tweepy.Cursor(api.user_timeline,user_id=user ,count=10,include_rts=True,exclude_replies=True).items(10):
+    for friends in userid:
+        for tweet in tweepy.Cursor(api.user_timeline,user_id=friends ,count=10,include_rts=True,exclude_replies=True).items(10):
             try:
             
                 #tweet.favorite()
