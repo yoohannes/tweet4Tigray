3868a9f76a46e5442daf830212d755f0a9e31ab8
yoohannes <yohannesgidey420@gmail.com>
2021-12-09 10:11:49 +0100

    error429

--- tweet.py
+++ tweet.py
@@ -16,28 +16,12 @@ def qouteHASH(hastag,items):
             tweet.retweet()
             likecount+=1
             print(str(likecount)+"tweets tweeted")
-            time.sleep(20)
+            
         except tweepy.TweepError as e:
             print(e.reason)
         except StopIteration:
             break
-#use drafts instead ,log it 2
-userid=[]
-for users in tweepy.Cursor(api.friends).items():
-        countretNlike=0
-        #print(users.name)
-        userid.append(users.id)
-def likeNretweet(userid):
-    for friends in userid:
-        for tweet in tweepy.Cursor(api.user_timeline,user_id=friends ,count=10,include_rts=True,exclude_replies=True).items(10):
-            try:
-            
-                #tweet.favorite()
-                tweet.retweet()
-                #print(tweet.text)
-                countretNlike=+1
-                print(str(countretNlike)+"done")
-                time.sleep(60)
-            except tweepy.TweepError as e:
-                print(e)        
+
+
+      
 qouteHASH('#TigrayGenocide',150)
